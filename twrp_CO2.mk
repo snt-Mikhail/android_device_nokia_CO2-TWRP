a677b657c6ffd0ffe60329cc3a6b8940898a58b1
snt-Mikhail <137252470+snt-Mikhail@users.noreply.github.com>
2023-06-22 16:36:40 +0700

    Update twrp_CO2.mk

--- twrp_CO2.mk
+++ twrp_CO2.mk
@@ -8,10 +8,10 @@
 COMMON_SOC := mt6755
 
 # Common path for device trees
-COMMON_PATH := device/nokia/$(COMMON_SOC)-common
+COMMON_PATH := device/nokia/$(COMMON_SOC)
 
 # Device path for OEM device tree
-DEVICE_PATH := device/nokia/CO2
+DEVICE_PATH := device/nokia/CO2_sprout
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
